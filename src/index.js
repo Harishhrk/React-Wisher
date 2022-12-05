ba2285b03f886e9a303ef5d2308e5dfae3cfce0d
Harishhrk <51930539+Harishhrk@users.noreply.github.com>
2022-12-05 18:23:10 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDom from "react-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const date = new Date();
+const date1 = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let day = date.getDate()
+let month = date.getMonth()
+let year = date.getFullYear()
+let min = date.getMinutes()
+let sec = date.getSeconds()
+let hour = date.getHours()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const cssStyle = { };
+var state = "";
+if(hour>=4 && hour<12){
+    state = "Good Morning"
+    cssStyle.color = "green";
+}
+else if( hour >=12 && hour <17){
+    state = " Good Afternoon"
+    cssStyle.color = "yellow"
+}
+else {
+    state="Good Evening"
+    cssStyle.color = "Blue"
+}
+ReactDom.render(
+    <>
+    <div>
+    <h1>Hello Sir, <span style={cssStyle}>{state}</span></h1>
+    </div>
+    
+    
+    </>
+    
+
+, document.getElementById("root"));
